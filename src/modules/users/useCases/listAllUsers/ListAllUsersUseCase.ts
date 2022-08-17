3ff108dbfed9e38be6483f3f27fc18e667a35d37
Vinicius Benedito <beneditoloura@gmail.com>
2022-08-17 07:27:00 -0300

    ✨ feat: create ListAllUsersUseCase execute method

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,6 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
+    const verifyIfUserIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!verifyIfUserIsAdmin.admin || !verifyIfUserIsAdmin) {
+      throw new Error("User isn't admin or not exists");
+    }
+
     const users = this.usersRepository.list();
     return users;
   }
