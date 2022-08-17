1fc9dcddb72b756c8fada6d7ff7f13a15d8d2067
Vinicius Benedito <beneditoloura@gmail.com>
2022-08-17 07:12:53 -0300

    ✨ feat: create ShowUserProfileUseCase execute method

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const getUserById = this.usersRepository.findById(user_id);
+
+    if (!getUserById) {
+      throw new Error("User id not found");
+    }
+
+    return getUserById;
   }
 }
 
