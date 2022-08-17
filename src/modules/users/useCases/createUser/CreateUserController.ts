30033dd8f08cba991bf36a66275cee966352cecd
Vinicius Benedito <beneditoloura@gmail.com>
2022-08-17 08:07:09 -0300

    ✨ feat: include try catch on CreateUserController handle method

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,11 +6,15 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { name, email } = request.body;
+    try {
+      const { name, email } = request.body;
 
-    const newUser = this.createUserUseCase.execute({ name, email });
+      const newUser = this.createUserUseCase.execute({ name, email });
 
-    return response.status(201).json(newUser);
+      return response.status(201).json(newUser);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
